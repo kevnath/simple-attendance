54e9964ccf481c8db19b03f566379b5607c858a4
Kevin Nathanael <Kevin Nathanael>
2016-04-29 22:45:04 +0700

    no message

--- repository/Repository.php
+++ repository/Repository.php
@@ -5,8 +5,8 @@
 		protected $db;
 
 		public function init() {
-			#$this->db = new PDO('mysql:host=ap-cdbr-azure-southeast-b.cloudapp.net;dbname=absensi;port:3306', 'b90ff032267402', '87acaf4f');
-			$this->db = new PDO('mysql:host=localhost;dbname=absensi;port=3306', 'root', '');
+			$this->db = new PDO('mysql:host=ap-cdbr-azure-southeast-b.cloudapp.net;dbname=absensi;port:3306', 'b90ff032267402', '87acaf4f');
+			#$this->db = new PDO('mysql:host=localhost;dbname=absensi;port=3306', 'root', '');
 			$this->db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
 		}
 
